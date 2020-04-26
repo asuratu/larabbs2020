77b3138d8bcee27ef2772795543ffa82856cdf38
antonytu <455224159@qq.com>
2020-04-26 12:05:40 +0800

    侧边栏链接

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,6 +9,7 @@ use App\Http\Requests\TopicRequest;
 use App\Handlers\ImageUploadHandler;
 use App\Models\Category;
 use App\Models\User;
+use App\Models\Link;
 use Auth;
 
 class TopicsController extends Controller
@@ -19,13 +20,15 @@ class TopicsController extends Controller
     }
 
 
-    public function index(Request $request, Topic $topic, User $user)
+    public function index(Request $request, Topic $topic, User $user, Link $link)
     {
         $topics = $topic->withOrder($request->order)
             ->with('user', 'category')  // 预加载防止 N+1 问题
             ->paginate(20);
         $active_users = $user->getActiveUsers();
-        return view('topics.index', compact('topics', 'active_users'));
+        $links = $link->getAllCached();
+
+        return view('topics.index', compact('topics', 'active_users', 'links'));
     }
 
     public function show(Request $request, Topic $topic)
